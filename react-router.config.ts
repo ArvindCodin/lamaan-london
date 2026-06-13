cd347b2bf5f5074b72db08b156e94be6ec7437c7
ArvindCodin <91817296+ArvindCodin@users.noreply.github.com>
2026-06-13 23:40:53 +0530

    delete all

--- react-router.config.ts
+++
@@ -1,6 +0,0 @@
-import type { Config } from "@react-router/dev/config";
-import { hydrogenPreset } from "@shopify/hydrogen/react-router-preset";
-
-export default {
-  presets: [hydrogenPreset()],
-} satisfies Config;
